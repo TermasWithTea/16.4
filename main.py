f15e257f6e79d8003d616240e0c69e0664270304
TermasWithTea <167928100+TermasWithTea@users.noreply.github.com>
2024-12-08 18:03:49 +0300

    main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ users = []
 class User(BaseModel):
     username: str
     age: int
+    id: int 
 
 @app.get('/users')
 async def get_reurn() -> list:
@@ -24,13 +25,15 @@ async def post_ret(username: str, age: int) -> User:
 async def put_ret(user_id: int, username: str, age: str) -> str:
     if user_id not in users:
         raise HTTPException(status_code = 404, detail = 'User not found')
-    users[user_id] = User(username = username, age = age)
+    user = User(username = username, age = age)
+    users[user_id] = user
     return f'The user {user_id} is update!'
 
 @app.delete('/user/{user_id}')
-async def del_del(user_id: int) -> dict:
+async def del_del(user_id: int) -> User:
     if user_id not in users:
-        raise HTTPException(status_code= 404, detail= 'User not found')
+        raise HTTPException(status_code=404, detail='User was not found')
+    
+    del_del = users[user_id]
     del users[user_id]
-    return {'message':f'User {user_id} has been deleted'}
-
+    return del_del
